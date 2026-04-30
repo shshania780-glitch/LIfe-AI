6b5650d524e5b77ea2fcc781d8465356fa4aaa8c
SHANIA <shshania780@gmail.com>
2026-04-30 10:35:36 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,72 +1,17 @@
-# SPDX-License-Identifier: MIT
+from __future__ import annotations
 
-from attr import (
-    NOTHING,
-    Attribute,
-    AttrsInstance,
-    Converter,
-    Factory,
-    NothingType,
-    _make_getattr,
-    assoc,
-    cmp_using,
-    define,
-    evolve,
-    field,
-    fields,
-    fields_dict,
-    frozen,
-    has,
-    make_class,
-    mutable,
-    resolve_types,
-    validate,
-)
-from attr._make import ClassProps
-from attr._next_gen import asdict, astuple, inspect
-
-from . import converters, exceptions, filters, setters, validators
-
+from .base import ANY
+from .base import default_namespace
+from .base import NamedSignal
+from .base import Namespace
+from .base import Signal
+from .base import signal
 
 __all__ = [
-    "NOTHING",
-    "Attribute",
-    "AttrsInstance",
-    "ClassProps",
-    "Converter",
-    "Factory",
-    "NothingType",
-    "__author__",
-    "__copyright__",
-    "__description__",
-    "__doc__",
-    "__email__",
-    "__license__",
-    "__title__",
-    "__url__",
-    "__version__",
-    "__version_info__",
-    "asdict",
-    "assoc",
-    "astuple",
-    "cmp_using",
-    "converters",
-    "define",
-    "evolve",
-    "exceptions",
-    "field",
-    "fields",
-    "fields_dict",
-    "filters",
-    "frozen",
-    "has",
-    "inspect",
-    "make_class",
-    "mutable",
-    "resolve_types",
-    "setters",
-    "validate",
-    "validators",
+    "ANY",
+    "default_namespace",
+    "NamedSignal",
+    "Namespace",
+    "Signal",
+    "signal",
 ]
-
-__getattr__ = _make_getattr(__name__)
